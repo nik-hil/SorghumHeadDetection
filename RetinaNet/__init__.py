d462acd8d218a0eefb8c774febd6e7fa65d35d60
Nikhil Ikhar <nikhil.ikhar@gmail.com>
2019-06-27 10:36:56 +0530

    Update __init__.py

--- RetinaNet/__init__.py
+++ RetinaNet/__init__.py
@@ -4,3 +4,9 @@
 #        COPPE - Universidade Federal do Rio de Janeiro                                   #
 #        Last modification: May 24th 2018                                                 #
 ###########################################################################################
+
+
+from os.path import dirname, basename, isfile, join
+import glob
+modules = glob.glob(join(dirname(__file__), "*.py"))
+__all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
